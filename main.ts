396d6c80d1a4503c560eeff5316378d40119a793
natalia <53310862+ripleycmd@users.noreply.github.com>
2021-09-19 17:05:44 +0100

    First cdk infra commit.

--- main.ts
+++ main.ts
@@ -1,14 +1,35 @@
-import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { Construct } from 'constructs'
+import { App, TerraformStack, Token} from 'cdktf'
+import { AwsProvider, Vpc, Subnet} from './.gen/providers/aws'
 
 class MyStack extends TerraformStack {
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
+  constructor(scope: Construct, id: string) {
+    super(scope, id)
 
-    // define resources here
+    new AwsProvider(this, 'aws', {
+      region: 'us-west-1',
+    })
+
+    const vpc = new Vpc(this, "my-tfcdk-vpc", {
+      cidrBlock: '10.0.0.0/16'
+    })
+
+    new Subnet(this, "my-tfcdk-subnet", {
+      vpcId: Token.asString(vpc.id),
+      cidrBlock: '10.0.0.0/16'
+    })
+
+    // const instance = new Instance(this, 'compute', {
+    //   ami: 'ami-01456a894f71116f2',
+    //   instanceType: 't2.micro',
+    // })
+
+    // new TerraformOutput(this, 'public_ip', {
+    //   value: instance.publicIp,
+    // })
   }
 }
 
-const app = new App();
-new MyStack(app, "cdktf-testdrive");
-app.synth();
+const app = new App()
+new MyStack(app, 'typescript-aws')
+app.synth()
