333cf768a00be3f4111ea9ee6c3881712de499fe
natalia <53310862+ripleycmd@users.noreply.github.com>
2021-09-20 14:07:04 +0100

    Added instances and resources names.

--- main.ts
+++ main.ts
@@ -1,32 +1,42 @@
 import { Construct } from 'constructs'
-import { App, TerraformStack, Token} from 'cdktf'
-import { AwsProvider, Vpc, Subnet} from './.gen/providers/aws'
+import { App, TerraformStack, Token, TerraformOutput} from 'cdktf'
+import { AwsProvider, Vpc, Subnet, Instance} from './.gen/providers/aws'
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id)
 
     new AwsProvider(this, 'aws', {
-      region: 'us-west-1',
+      region: 'eu-west-1',
     })
 
     const vpc = new Vpc(this, "my-tfcdk-vpc", {
-      cidrBlock: '10.0.0.0/16'
+      cidrBlock: '10.0.0.0/16',
+      tags: {
+        "Name": "cdktf-vpc"
+      }
     })
 
     new Subnet(this, "my-tfcdk-subnet", {
       vpcId: Token.asString(vpc.id),
-      cidrBlock: '10.0.0.0/16'
+      cidrBlock: '10.0.0.0/16',
+      tags: {
+        "Name": "cdktf-subnet"
+      }
     })
 
-    // const instance = new Instance(this, 'compute', {
-    //   ami: 'ami-01456a894f71116f2',
-    //   instanceType: 't2.micro',
-    // })
+    const instance = new Instance(this, 'compute', {
+      ami: 'ami-01456a894f71116f2',
+      instanceType: 't2.micro',
+      tags: {
+        "Name": "cdktf-instance"
+      }
+        
+    })
 
-    // new TerraformOutput(this, 'public_ip', {
-    //   value: instance.publicIp,
-    // })
+    new TerraformOutput(this, 'public_ip', {
+      value: instance.publicIp,
+    })
   }
 }
 
